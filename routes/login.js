146f2319af6183ac97be8c4c94fb057269a66272
maheshtestriq <131264969+maheshtestriq@users.noreply.github.com>
2024-05-17 17:38:24 +0530

    id field

--- routes/login.js
+++ routes/login.js
@@ -20,42 +20,16 @@ connection.connect((err) => {
   console.log('Connected to MySQL as id ' + connection.threadId);
 });
 
-
-// app.post('/login', (req, res) => {
-
-
-//   const sql = "SELECT * FROM users WHERE `email` =? AND `password`=?";
-//   db.query(sql, [req.body.email,req.body.password], (err, result) => {
-//     if (err) {
-//    return res.json("Error");
-
-//     }
-//     if (data.length>0){
-
-//         const id= data[0].id;
-//         const token =jwt.sign({id},"jwtSecretKey",{expiresIn:300});
-//         return res.json({Login: true, token, result});
-//     }
-//     else{
-//         return res.json("Failed")
-//     }
-
-//   });
-// });
-
-
-
 const bcrypt = require('bcrypt');
 
-
-module.exports = (db) => {
+module.exports = () => { // Removed `db` parameter as it's not being used
   const router = express.Router();
 
   router.post('/login', (req, res) => {
     const { email, password } = req.body;
 
     const sql = "SELECT * FROM users WHERE `email` = ?";
-    connection.query(sql, [email], (err, result) => {
+    connection.query(sql, [email], (err, result) => { // Use `connection` object instead of `db`
       if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Internal server error' });
@@ -71,7 +45,16 @@ module.exports = (db) => {
 
           if (isValid) {
             const token = jwt.sign({ id: user.id }, "jwtSecretKey", { expiresIn: 300 });
-            return res.json({ success: true, message: 'Login successful', token, user });
+            return res.json({
+              success: true,
+              message: 'Login successful',
+              token,
+              user: {
+                id: user.id,
+                username: user.username,
+                email: user.email
+              }
+            });
           } else {
             return res.status(401).json({ success: false, error: 'Invalid email or password' });
           }
@@ -84,33 +67,3 @@ module.exports = (db) => {
 
   return router;
 };
-
-
-
-// const jwt = require('jsonwebtoken');
-
-// module.exports = (db) => {
-//   const router = express.Router();
-
-//   router.post('/login', (req, res) => {
-//     const { email, password } = req.body;
-
-//     const sql = "SELECT * FROM users WHERE `email` = ? AND `password` = ?";
-//     connection.query(sql, [email, password], (err, result) => {
-//       if (err) {
-//         console.error(err);
-//         return res.status(500).json({ error: 'Internal server error' });
-//       }
-
-//       if (result.length > 0) {
-//         const user = result[0];
-//         const token = jwt.sign({ id: user.id }, "jwtSecretKey", { expiresIn: 300 });
-//         return res.json({ success: true, message: 'Login successful', token, user });
-//       } else {
-//         return res.status(401).json({ success: false, error: 'Invalid email or password' });
-//       }
-//     });
-//   });
-
-//   return router;
-// };
